0e49dfa72d3eb3ed93e202cf7ae517dae72f310c
Kirill Yudanov <yudanovkr@yandex.ru>
2025-04-04 13:29:56 +0300

    Переделано под DefaultRouter

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,23 @@
-from django.urls import path
-from .views import ReviewListAPIView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView, TokenVerifyView
+)
+
+from .views import (
+    ReviewViewSet,
+    TitleViewSet,
+)
+
+router = DefaultRouter()
+router.register(r'titles', TitleViewSet, basename='titles')
+router.register(r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='reviews')
 
 
 urlpatterns = [
-    path('titles/<int:title_id>/reviews/', ReviewListAPIView.as_view(), name='review-list'), #пока для теста потом переделаем на динамические маршруты
+    path('v1/jwt/create/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('v1/jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('v1/jwt/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('v1/', include(router.urls)),
 ]
