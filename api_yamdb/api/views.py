ea835ac26e2ac1c0ae6e9c35763a0f2e353f735c
Kirill Yudanov <yudanovkr@yandex.ru>
2025-04-04 13:28:39 +0300

    Переделано на вьювсеты

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,16 +1,33 @@
-from rest_framework import generics
-from rest_framework.exceptions import NotFound
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, permissions
+
 from reviews.models import Title, Review
-from .serializers import ReviewSerializer
+from .serializers import ReviewSerializer, TitleSerializer
+from .premission import IsOwnerOrReadOnly
 
 
-class ReviewListAPIView(generics.ListAPIView):
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly
+    ]
+
+    def get_title(self):
+        return get_object_or_404(Title, pk=self.kwargs['title_id'])
 
     def get_queryset(self):
-        title_id = self.kwargs.get('title_id')
-        try:
-            title = Title.objects.get(id=title_id)
-        except Title.DoesNotExist:
-            raise NotFound(detail="Произведение не найдено")
-        return title.reviews.all()
+        return self.get_title().reviews.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, title=self.get_title())
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly
+    ]
