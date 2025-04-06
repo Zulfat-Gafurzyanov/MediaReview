ffb8354e0b906a7f71736564622edd7e04183b63
Zulfat <gafurzyanov.zulfat@gmail.com>
2025-04-06 14:36:18 +1000

    Добавил команду на загрузку данных из csv файла.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -9,10 +9,10 @@ router_v1 = SimpleRouter()
 
 router_v1.register('categories', CategoryViewSet, basename='categories')
 router_v1.register('genres', GenreViewSet, basename='genres')
-router_v1.register(r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet)
+router_v1.register(r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='reviews')
 router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<reviews_id>\d+)/comments',
-    CommentViewSet)
+    CommentViewSet, basename='comment')
 
 urlpatterns = [
     path(f'{APIVERSION}/', include(router_v1.urls))
