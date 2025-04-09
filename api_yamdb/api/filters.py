0ad5e0fa38d477593dc089a740dc0551ae7644ce
Kirill Yudanov <yudanovkr@yandex.ru>
2025-04-09 09:39:48 +0300

    test passed

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -3,7 +3,7 @@ from reviews.models import Title
 
 
 class TitleFilter(django_filters.FilterSet):
-    genre = django_filters.CharFilter(field_name='genre__slug', lookup_expr='iexact')
+    genre = django_filters.CharFilter(field_name='genres__slug', lookup_expr='iexact')
     category = django_filters.CharFilter(field_name='category__slug', lookup_expr='iexact')
     name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
     year = django_filters.NumberFilter(field_name='year')
