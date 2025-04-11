482f785f25d0bb21687f19ea032d2233852742ba
Baronofff <Baronov.ev@gmail.com>
2025-04-11 10:55:49 +0300

    bugfixes

--- api_yamdb/api/validators.py
+++ api_yamdb/api/validators.py
@@ -2,20 +2,15 @@ from datetime import datetime
 
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.core.exceptions import ValidationError
-from django.core.validators import ValidationError as DjangoValidationError
-from rest_framework import serializers
 
 from api.constants import NOT_ALLOWED_USERNAME
 
 
 def user_validator(value):
     """Валидатор для проверки имени пользователя."""
-    if value.lower() in NOT_ALLOWED_USERNAME:
+    if value in NOT_ALLOWED_USERNAME:
         raise ValidationError(f'{value} не может стать именем пользователя.')
-    try:
-        UnicodeUsernameValidator()(value)
-    except DjangoValidationError as e:
-        raise serializers.ValidationError(e.message)
+    UnicodeUsernameValidator()(value)
 
 
 def title_year_validator(value):
@@ -24,5 +19,3 @@ def title_year_validator(value):
     if value > now:
         raise ValidationError(
             f'Год выпуска произведения не может быть больше {now} года')
-    
-
